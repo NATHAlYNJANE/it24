6da3bef088066e9ad0951258a493d121edec338c
nathalyn <nat@gmail.com>
2024-09-05 02:35:08 -1200

    Functionality 3 - Check if a string is a palindrome

--- app.js
+++ app.js
@@ -54,3 +54,16 @@ function chooseAction(option) {
 }
 
 chooseAction(1);
+
+function isPalindrome(str) {
+    let len = str.length;
+    for (let i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let testString = "madam";
+console.log(isPalindrome(testString) ? testString + " is a Palindrome" : testString + " is not a Palindrome");
