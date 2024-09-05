7e162455a55838cfdb944e187b035e4afeba1301
nathalyn <nat@gmail.com>
2024-09-05 02:52:25 -1200

    update the app js

--- app.js
+++ app.js
@@ -1,69 +1,53 @@
-console.log("App Started: Data Types, Operators, Conditionals, and Loops");
+console.log("App Started: UI Integrated");
+ 
+function displayResult(message) {
+    document.getElementById('result').innerText = message;
+}
 
-let num1 = 10;     
-let num2 = 5;      
-let message = "Hello, World!";  
-
-let sum = num1 + num2;
-let difference = num1 - num2;
-let product = num1 * num2;
-let quotient = num1 / num2;
-
-console.log("Sum: " + sum);
-console.log("Difference: " + difference);
-console.log("Product: " + product);
-console.log("Quotient: " + quotient);
-
-
-function checkOddOrEven(num) {
+function checkOddOrEven() {
+    let num = parseInt(document.getElementById('numberInput').value);
+    if (isNaN(num)) {
+        displayResult("Please enter a valid number!");
+        return;
+    }
     if (num % 2 === 0) {
-        console.log(num + " is Even");
+        displayResult(num + " is Even");
     } else {
-        console.log(num + " is Odd");
+        displayResult(num + " is Odd");
     }
 }
-
-checkOddOrEven(7); 
-function isPrime(num) {
-    if (num <= 1) return false;
+e
+function checkPrime() {
+    let num = parseInt(document.getElementById('numberInput').value);
+    if (isNaN(num)) {
+        displayResult("Please enter a valid number!");
+        return;
+    }
+    if (num <= 1) {
+        displayResult(num + " is not a Prime Number");
+        return;
+    }
     for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) return false;
-    }
-    return true;
-}
-
-let number = 11;
-if (isPrime(number)) {
-    console.log(number + " is a Prime Number");
-} else {
-    console.log(number + " is not a Prime Number");
-}
-
-function chooseAction(option) {
-    switch (option) {
-        case 1:
-            checkOddOrEven(8);
-            break;
-        case 2:
-            let primeNumber = 13;
-            console.log(isPrime(primeNumber) ? primeNumber + " is Prime" : primeNumber + " is not Prime");
-            break;
-        default:
-            console.log("Invalid Option");
-    }
-}
-
-chooseAction(1);  
-
-function isPalindrome(str) {
-    let len = str.length;
-    for (let i = 0; i < len / 2; i++) {
-        if (str[i] !== str[len - 1 - i]) {
-            return false;
+        if (num % i === 0) {
+            displayResult(num + " is not a Prime Number");
+            return;
         }
     }
-    return true;
+    displayResult(num + " is a Prime Number");
 }
 
-let testString = "madam";
-console.log(isPalindrome(testString) ? testString + " is a Palindrome" : testString + " is not a Palindrome");
+function checkPalindrome() {
+    let str = document.getElementById('stringInput').value;
+    if (!str) {
+        displayResult("Please enter a valid string!");
+        return;
+    }
+    let len = str.length;
+    for (let i = 0; i < len / 2; i++) {
+        if (str[i].toLowerCase() !== str[len - 1 - i].toLowerCase()) {
+            displayResult(str + " is not a Palindrome");
+            return;
+        }
+    }
+    displayResult(str + " is a Palindrome");
+}
