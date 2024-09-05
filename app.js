d55ca4eb878e88a4c819b4e5252892dc6e1f3f44
nathalyn <nat@gmail.com>
2024-09-05 02:32:33 -1200

    Functionality 2 - Check if a number is prime

--- app.js
+++ app.js
@@ -23,3 +23,18 @@ function checkOddOrEven(num) {
 }
 
 checkOddOrEven(7); 
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+let number = 11;
+if (isPrime(number)) {
+    console.log(number + " is a Prime Number");
+} else {
+    console.log(number + " is not a Prime Number");
+}
